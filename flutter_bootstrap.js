94a59456ba973700cd041baf44d58d1821e30ddd
IsaacMindustry <IsaacMindustry@users.noreply.github.com>
2026-05-20 18:23:51 +0000

    deploy: fd9531cad11e692d3695c7c185ca52aa822f28f9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2080288882" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4281677723" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
